9a7d27cfdafa82bc3697de638b528b17f7b424d8
Yuri Aksenov <aksenov.mailbox@gmail.com>
2017-10-23 11:55:38 +0300

    Скрипт карусели для десктопов

--- scripts/script.js
+++ scripts/script.js
@@ -13,4 +13,36 @@ $(".catalog-of-shops__menu-item > a").click(function(e) {
 
 /* Our clients carousel */
 
+$(".our-clients__carousel-button--forward").click(function() {
+  $(".our-clients__carousel-button--back").removeAttr("disabled");
 
+  for (var i = 0; i < $(".catalog-of-shops__carousel-item").length; i++) {
+    if ($($(".catalog-of-shops__carousel-item")[i]).hasClass("catalog-of-shops__carousel-item--visible")) {
+      $($(".catalog-of-shops__carousel-item")[i]).removeClass("catalog-of-shops__carousel-item--visible");
+      $($(".catalog-of-shops__carousel-item")[i + 2]).css("marginRight", "5px");
+      $($(".catalog-of-shops__carousel-item")[i + 3]).addClass("catalog-of-shops__carousel-item--visible");
+      $($(".catalog-of-shops__carousel-item")[i + 3]).css("marginRight", 0);
+      if (i == 3) {
+        $(".our-clients__carousel-button--forward").attr("disabled", "disabled");
+      }
+      break;
+    }    
+  }
+});
+
+$(".our-clients__carousel-button--back").click(function() {
+  $(".our-clients__carousel-button--forward").removeAttr("disabled");
+
+  for (var i = 0; i < $(".catalog-of-shops__carousel-item").length; i++) {
+    if ($($(".catalog-of-shops__carousel-item")[i]).hasClass("catalog-of-shops__carousel-item--visible")) {
+      $($(".catalog-of-shops__carousel-item")[i - 1]).addClass("catalog-of-shops__carousel-item--visible");
+      $($(".catalog-of-shops__carousel-item")[i + 1]).css("marginRight", 0);
+      $($(".catalog-of-shops__carousel-item")[i + 2]).removeClass("catalog-of-shops__carousel-item--visible");
+      $($(".catalog-of-shops__carousel-item")[i + 2]).css("marginRight", "5px");
+      if (i == 1) {
+        $(".our-clients__carousel-button--back").attr("disabled", "disabled");
+      }
+      break;
+    }    
+  }
+});
